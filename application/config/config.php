87fb1a0b7ddbbbd70826655cc6960117b5ef8524
onlinesecrets <jonnifer.villason@gmail.com>
2014-02-21 02:32:59 +0800

    new changes

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/teampora';
+$config['base_url']	= 'http://teampora.gopagoda.com/';
 
 /*
 |--------------------------------------------------------------------------
